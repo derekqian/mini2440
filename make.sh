6c720ef7ea8009202ad9c19777064d60f27ddda2
derekqian <electronseu@gmail.com>
2012-12-19 12:02:20 -0800

    tried another toolchain suggested. no luck. maybe because we are running it on a 64-bit machine

--- make.sh
+++ make.sh
@@ -1,21 +1,38 @@
 # http://code.google.com/p/friendlyarm/wiki/Linux_Tutorial
 
+#####################################################
+# build qemu
 git clone git://repo.or.cz/qemu/mini2440.git qemu
 cd qemu
 ./configure --target-list=arm-softmmu
 make -j4
 cd ..
 
-sudo add-apt-repository ppa:linaro-maintainers/toolchain
-sudo apt-get update
-sudo apt-get install gcc-arm-linux-gnueabi g++-arm-linux-gnueabi
-#sudo apt-get install gcc-arm-linux-gnueabi
+#####################################################
+# build uboot
+
+# doesn't work for uboot
+#sudo add-apt-repository ppa:linaro-maintainers/toolchain
+#sudo apt-get update
+#sudo apt-get install gcc-arm-linux-gnueabi g++-arm-linux-gnueabi
+
+#mkdir -p ~/dev/toolchain
+mkdir toolchain
+cd toolchain
+wget http://files.chumby.com/toolchain/arm-2008q3-72-arm-none-linux-gnueabi-i686-pc-linux-gnu.tar.bz2
+tar xjvf arm-2008q3-72-arm-none-linux-gnueabi-i686-pc-linux-gnu.tar.bz2
+export PATH=/home/derek/mini2440/toolchain/arm-2008q3/bin:$PATH
 
 git clone git://repo.or.cz/u-boot-openmoko/mini2440.git uboot
 cd uboot
 #Open Makefile, change CROSS_COMPILE to appropriate value.
-make mini2440_config
-make -j4
+#make mini2440_config
+make mini2440_config CROSS_COMPILE=arm-none-linux-gnueabi-
+#make -j4
+make -j4 CROSS_COMPILE=arm-none-linux-gnueabi-
+
+
+
 
 
 
